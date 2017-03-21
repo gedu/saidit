a1c4f8c6ba2e457d405890fb28b6c5a10c92653f
edug <edu@funler.com>
2017-03-21 11:35:19 -0300

    basic app structure

---
+++ Saidit/app/src/main/java/com/gemapps/saidit/ui/toplisting/TopListingActivity.java
@@ -0,0 +1,15 @@
+package com.gemapps.saidit.ui.toplisting;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.gemapps.saidit.R;
+
+public class TopListingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_top_listing);
+    }
+}
